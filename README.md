3bc8953a881aaf30c5e68567f96a269d4d1e1059
pavel <plaureanopena@gmail.com>
2022-02-28 18:28:44 -0500

    Conclude game

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # rock-paper-scissors
-Recreating a game of Rock, Paper, Scissors.
+Recreating a game of Rock, Paper, Scissors completely on my own, before reading guidance or instructions. 
--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-let computerPlay = (playerChoice) => {
+let RPS = (playerChoice) => {
     //Code to randomly choose between Rock, paper, or scissors
     let rng = Math.floor(Math.random()*3);
    if (rng === 0) {
