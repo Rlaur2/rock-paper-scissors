cfd93b2784b9f806e8817342cb489427e97e4cd7
pavel <plaureanopena@gmail.com>
2022-02-28 17:13:54 -0500

    Create random function that chooses between RPS

---
+++ rps.js
@@ -0,0 +1,11 @@
+let computerPlay = () => {
+   let choice = Math.floor(Math.random()*3);
+   if (choice === 0) {
+       choice = 'Rock';
+   } else if (choice === 1) {
+       choice = 'Paper';
+   } else {
+       choice = 'Scissors';
+   }
+   return choice
+}
