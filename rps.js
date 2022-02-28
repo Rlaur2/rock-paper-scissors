27f3db7e9018559b6a69e18e36b24767553bdef9
pavel <plaureanopena@gmail.com>
2022-02-28 17:39:37 -0500

    Create AND/OR statements to determine winner

--- rps.js
+++ rps.js
@@ -1,12 +1,14 @@
-let computerPlay = () => {
-    let choice;
-   let rng = Math.floor(Math.random()*3);
+let computerPlay = (playerChoice) => {
+    let rng = Math.floor(Math.random()*3);
    if (rng === 0) {
-       choice = 'Rock';
+       aiChoice = 'Rock';
    } else if (rng === 1) {
-       choice = 'Paper';
+       aiChoice = 'Paper';
    } else {
-       choice = 'Scissors';
+       aiChoice = 'Scissors';
    }
-   return choice;
+   
+   if (playerChoice === 'Rock' && aiChoice === 'Scissors' || playerChoice === 'Paper' && aiChoice === 'Rock' || playerChoice === 'Scissors' && aiChoice === 'Paper') {
+       console.log(`You chose ${playerChoice} and your opponent chose ${aiChoice}. You win!`)
+   }
 }
